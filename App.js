6c9fa7b5ec55513cae4da826272b206f369b565a
sgraffite <sgraffite+github@gmail.com>
2019-03-25 12:22:13 -0600

    Refactored app to use this

--- App.js
+++ App.js
@@ -3,33 +3,60 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     launch: function() {
         console.log('Tacos are yummy!');
-        this._loadData();
+        //this._loadData();
+        this._loadIterations();
+    },
+
+    // Load iterations
+    _loadIterations: function(){
+        this.iterationsComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+            listeners: {
+                ready: function(comboBox){
+                    this._loadData();
+                },
+                select: function(comboBox, records) {
+                    console.log(records);
+                    this._loadData();
+                },
+                scope: this
+            },
+        });
+        this.add(this.iterationsComboBox);
     },
 
     // Load data
     _loadData: function(){
-        let myStore = Ext.create('Rally.data.wsapi.Store', {
-            model: 'User Story',
+        let selectedIteration = this.iterationsComboBox.getRecord().get('_ref');
+        console.log('selectedIteration', selectedIteration);
+        this.store = Ext.create('Rally.data.wsapi.Store', {
+            model: 'Defect',
             autoLoad: true,
+            filters:[
+                {
+                    property: 'Iteration',
+                    operation: '=',
+                    value: selectedIteration
+                }
+            ],
             listeners: {
                 load: function(store, data, success) {
                     console.log('got data!', data, success);
-                    this._loadGrid(store);
+                    this._loadGrid();
                 },
                 scope: this
             },
-            fetch: ['FormattedID', 'Name', 'ScheduleState']
+            fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']
         });
     },
 
     // Load grid
-    _loadGrid: function(store){
-        var myGrid = Ext.create('Rally.ui.grid.Grid', {
-            store: store,
+    _loadGrid: function(){
+        this.grid = Ext.create('Rally.ui.grid.Grid', {
+            store: this.store,
             columnCfgs: [
-                'FormattedID', 'Name', 'ScheduleState'
+                'FormattedID', 'Name', 'Severity', 'Iteration'
             ]
         });
-        this.add(myGrid);
+        this.add(this.grid);
     }
 });
